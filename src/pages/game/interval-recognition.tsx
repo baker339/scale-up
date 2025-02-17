f1f5d098dfdf21d1664fc7fe992477fd1ce5a26f
Aidan Baker <aidan.baker@dhcs.ca.gov>
2025-02-17 15:36:22 -0500

    fixed some errors on deploy

--- src/pages/game/interval-recognition.tsx
+++ src/pages/game/interval-recognition.tsx
@@ -108,7 +108,7 @@ export default function IntervalRecognition() {
         stave.addClef('treble').setContext(context).draw();
 
         const rootNote = 'c/4'; // Fixed root note.
-        // @ts-ignore
+        // @ts-expect-error
         const secondNoteRaw = intervalMapping[intervalObj.name] || 'g4';
         const secondNote = formatNote(secondNoteRaw);
 
